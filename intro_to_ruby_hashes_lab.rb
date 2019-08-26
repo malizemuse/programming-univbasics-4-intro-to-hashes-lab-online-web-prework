dd1fb7c7633a13daf8f3f584bd40c2d3ea258641
gregarious-number-3828 <muse.malize@gmail.com>
2019-08-26 20:40:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,8 +23,8 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  key_name = key
-  {key_name: value}
+  hash[key] = value
+  hash
 end
 
 def read_from_hash(hash, key)
